6bd12fa93eef251c6025796e05c018a2349de766
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-22 17:09:00 +0530

    Configure server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const { sequelize } = require('./models');
-const db = require('./models/init-models')(sequelize);
-console.log(db);
+
+const roleRouter = require('./routes/roles');
+const userRouter = require('./routes/users');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/roles', roleRouter);
+app.use('/api/users', userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
