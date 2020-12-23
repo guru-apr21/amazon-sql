f4930e2f2cc82995ec781b7ab9dde2604dc61a10
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-23 14:13:21 +0530

    Define static and instance method To generate auth token and validate sign in

--- models/User.js
+++ models/User.js
@@ -1,78 +1,95 @@
 const Sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 module.exports = (sequelize, DataTypes) => {
   return User.init(sequelize, DataTypes);
-}
+};
 
 class User extends Sequelize.Model {
   static init(sequelize, DataTypes) {
-  super.init({
-    user_id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    first_name: {
-      type: DataTypes.STRING(50),
-      allowNull: false
-    },
-    last_name: {
-      type: DataTypes.STRING(50),
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: "email_UNIQUE"
-    },
-    date_registered: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    encrypted_password: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    role_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 2,
-      references: {
-        model: 'roles',
-        key: 'role_id'
+    super.init(
+      {
+        user_id: {
+          autoIncrement: true,
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+        },
+        first_name: {
+          type: DataTypes.STRING(50),
+          allowNull: false,
+        },
+        last_name: {
+          type: DataTypes.STRING(50),
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+          unique: 'email_UNIQUE',
+        },
+        date_registered: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        encrypted_password: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        role_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 2,
+          references: {
+            model: 'roles',
+            key: 'role_id',
+          },
+        },
+      },
+      {
+        sequelize,
+        tableName: 'users',
+        timestamps: false,
+        indexes: [
+          {
+            name: 'PRIMARY',
+            unique: true,
+            using: 'BTREE',
+            fields: [{ name: 'user_id' }],
+          },
+          {
+            name: 'email_UNIQUE',
+            unique: true,
+            using: 'BTREE',
+            fields: [{ name: 'email' }],
+          },
+          {
+            name: 'fk_users_roles_idx',
+            using: 'BTREE',
+            fields: [{ name: 'role_id' }],
+          },
+        ],
       }
-    }
-  }, {
-    sequelize,
-    tableName: 'users',
-    timestamps: false,
-    indexes: [
+    );
+    return User;
+  }
+
+  genAuthToken() {
+    return jwt.sign(
+      { user_id: this.user_id, email: this.email },
+      'jwtsecretkey',
       {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "user_id" },
-        ]
-      },
-      {
-        name: "email_UNIQUE",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "email" },
-        ]
-      },
-      {
-        name: "fk_users_roles_idx",
-        using: "BTREE",
-        fields: [
-          { name: "role_id" },
-        ]
-      },
-    ]
-  });
-  return User;
+        expiresIn: '48h',
+      }
+    );
+  }
+
+  static validateSignIn(value) {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    });
+    return schema.validate(value);
   }
 }
