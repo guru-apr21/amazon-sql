fa747e8680bcbb6d4fb9b596130a3026fcc81f9a
Guru Prakash Ram <guruprakashram99@gmail.com>
2020-12-28 20:34:35 +0530

    Define route to update,retrieve existing products.

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,16 @@ const { sequelize } = require('../models');
 
 const db = require('../models/init-models')(sequelize);
 
+const getProducts = async (req, res, next) => {
+  try {
+    const products = await db.Product.findAll();
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Something went wrong!' });
+  }
+};
+
 const createProduct = async (req, res, next) => {
   try {
     const { error } = db.Product.validateNewProduct(req.body);
@@ -35,4 +45,30 @@ const createProduct = async (req, res, next) => {
   }
 };
 
-module.exports = { createProduct };
+const updateProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    let product = await db.Product.findByPk(id);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found!' });
+    }
+
+    const { user_id, role_id } = req.loggedInUser;
+    const hasAccessToProduct = product.user_id === user_id && role_id !== 1;
+    if (!hasAccessToProduct) {
+      return res.status(403).json({ error: 'Access denied!' });
+    }
+
+    product = await product.update({ ...req.body }, null, {
+      skip: ['user_id'],
+    });
+
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Something went wrong!' });
+  }
+};
+
+module.exports = { createProduct, updateProduct, getProducts };
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { productController } = require('../controllers');
 const isLoggedIn = require('../middlewares/auth');
 
+router.get('/', productController.getProducts);
+
 router.post('/', isLoggedIn, productController.createProduct);
+
+router.put('/:id', isLoggedIn, productController.updateProduct);
 
 module.exports = router;
