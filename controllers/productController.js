116cab67bdd35425bc18c54f160865a6e007469a
Guru Prakash Ram <guruprakashram99@gmail.com>
2020-12-28 16:35:40 +0530

    Define method to create new peoduct.

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,28 +3,36 @@ const { sequelize } = require('../models');
 const db = require('../models/init-models')(sequelize);
 
 const createProduct = async (req, res, next) => {
-  //   const { user_id, email } = req.loggedInUser;
-  const user = await db.User.findOne({
-    where: { email: 'guru.apr21@gmail.com' },
-    include: db.Role,
-  });
-  console.log(user);
+  try {
+    const { error } = db.Product.validateNewProduct(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
 
-  if (user.Role.name === 'buyer') {
-    return res.status(403).json({ error: 'Access denied!' });
+    const { user_id } = req.loggedInUser;
+    console.log(user_id);
+
+    const { title, quantity_in_stock, unit_price, category_id } = req.body;
+
+    const category = await db.Category.findByPk(category_id);
+
+    if (!category) {
+      return res.status(404).json({ error: "Category doesn't exist!" });
+    }
+
+    const product = await db.Product.create({
+      title,
+      quantity_in_stock,
+      unit_price,
+      user_id,
+      category_id,
+    });
+
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Something went wrong!' });
   }
-
-  const { title, quantity_in_stock, unit_price, category_id } = req.body;
-
-  const product = db.Product.create({
-    title,
-    quantity_in_stock,
-    unit_price,
-    user_id: user.user_id,
-    category_id,
-  });
-
-  res.json(product.toJSON());
 };
 
 module.exports = { createProduct };
--- models/Product.js
+++ models/Product.js
@@ -1,75 +1,82 @@
 const Sequelize = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   return Product.init(sequelize, DataTypes);
-}
+};
 
 class Product extends Sequelize.Model {
   static init(sequelize, DataTypes) {
-  super.init({
-    product_id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      comment: "Maintain product details"
-    },
-    title: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    quantity_in_stock: {
-      type: DataTypes.SMALLINT,
-      allowNull: false
-    },
-    unit_price: {
-      type: DataTypes.DECIMAL(9,2),
-      allowNull: false
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'user_id'
+    super.init(
+      {
+        product_id: {
+          autoIncrement: true,
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          comment: 'Maintain product details',
+        },
+        title: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        quantity_in_stock: {
+          type: DataTypes.SMALLINT,
+          allowNull: false,
+        },
+        unit_price: {
+          type: DataTypes.DECIMAL(9, 2),
+          allowNull: false,
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'users',
+            key: 'user_id',
+          },
+        },
+        category_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'categories',
+            key: 'category_id',
+          },
+        },
+      },
+      {
+        sequelize,
+        tableName: 'products',
+        timestamps: false,
+        indexes: [
+          {
+            name: 'PRIMARY',
+            unique: true,
+            using: 'BTREE',
+            fields: [{ name: 'product_id' }],
+          },
+          {
+            name: 'fk_products_users1_idx',
+            using: 'BTREE',
+            fields: [{ name: 'user_id' }],
+          },
+          {
+            name: 'fk_products_categories1_idx',
+            using: 'BTREE',
+            fields: [{ name: 'category_id' }],
+          },
+        ],
       }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'categories',
-        key: 'category_id'
-      }
-    }
-  }, {
-    sequelize,
-    tableName: 'products',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "product_id" },
-        ]
-      },
-      {
-        name: "fk_products_users1_idx",
-        using: "BTREE",
-        fields: [
-          { name: "user_id" },
-        ]
-      },
-      {
-        name: "fk_products_categories1_idx",
-        using: "BTREE",
-        fields: [
-          { name: "category_id" },
-        ]
-      },
-    ]
-  });
-  return Product;
+    );
+    return Product;
+  }
+
+  static validateNewProduct(value) {
+    const schema = Joi.object({
+      title: Joi.string().required(),
+      quantity_in_stock: Joi.number().required(),
+      unit_price: Joi.number().required(),
+      category_id: Joi.number().required(),
+    });
+    return schema.validate(value);
   }
 }
