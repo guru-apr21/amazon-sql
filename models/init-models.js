63bde9ca4925505ee91b13817da4bc31dffd1da0
Guru Prakash Ram <guruprakashram99@gmail.com>
2020-12-28 10:29:07 +0530

    Refactor code.

--- models/init-models.js
+++ models/init-models.js
@@ -1,12 +1,12 @@
-var DataTypes = require("sequelize").DataTypes;
-var _CartItem = require("./CartItem");
-var _Category = require("./Category");
-var _OrderItem = require("./OrderItem");
-var _OrderStatus = require("./OrderStatus");
-var _Order = require("./Order");
-var _Product = require("./Product");
-var _Role = require("./Role");
-var _User = require("./User");
+var DataTypes = require('sequelize').DataTypes;
+var _CartItem = require('./CartItem');
+var _Category = require('./Category');
+var _OrderItem = require('./OrderItem');
+var _OrderStatus = require('./OrderStatus');
+var _Order = require('./Order');
+var _Product = require('./Product');
+var _Role = require('./Role');
+var _User = require('./User');
 
 function initModels(sequelize) {
   var CartItem = _CartItem(sequelize, DataTypes);
@@ -18,28 +18,44 @@ function initModels(sequelize) {
   var Role = _Role(sequelize, DataTypes);
   var User = _User(sequelize, DataTypes);
 
-  User.belongsToMany(Product, { through: CartItem, foreignKey: "user_id", otherKey: "product_id" });
-  Product.belongsToMany(User, { through: CartItem, foreignKey: "product_id", otherKey: "user_id" });
-  Product.belongsToMany(Order, { through: OrderItem, foreignKey: "product_id", otherKey: "order_id" });
-  Order.belongsToMany(Product, { through: OrderItem, foreignKey: "order_id", otherKey: "product_id" });
-  CartItem.belongsTo(Product, { foreignKey: "product_id"});
-  Product.hasMany(CartItem, { foreignKey: "product_id"});
-  CartItem.belongsTo(User, { foreignKey: "user_id"});
-  User.hasMany(CartItem, { foreignKey: "user_id"});
-  OrderItem.belongsTo(Order, { foreignKey: "order_id"});
-  Order.hasMany(OrderItem, { foreignKey: "order_id"});
-  OrderItem.belongsTo(Product, { foreignKey: "product_id"});
-  Product.hasMany(OrderItem, { foreignKey: "product_id"});
-  Order.belongsTo(OrderStatus, { foreignKey: "status"});
-  OrderStatus.hasMany(Order, { foreignKey: "status"});
-  Order.belongsTo(User, { foreignKey: "user_id"});
-  User.hasMany(Order, { foreignKey: "user_id"});
-  Product.belongsTo(Category, { foreignKey: "category_id"});
-  Category.hasMany(Product, { foreignKey: "category_id"});
-  Product.belongsTo(User, { foreignKey: "user_id"});
-  User.hasMany(Product, { foreignKey: "user_id"});
-  User.belongsTo(Role, { foreignKey: "role_id"});
-  Role.hasMany(User, { foreignKey: "role_id"});
+  User.belongsToMany(Product, {
+    through: CartItem,
+    foreignKey: 'user_id',
+    otherKey: 'product_id',
+  });
+  Product.belongsToMany(User, {
+    through: CartItem,
+    foreignKey: 'product_id',
+    otherKey: 'user_id',
+  });
+  Product.belongsToMany(Order, {
+    through: OrderItem,
+    foreignKey: 'product_id',
+    otherKey: 'order_id',
+  });
+  Order.belongsToMany(Product, {
+    through: OrderItem,
+    foreignKey: 'order_id',
+    otherKey: 'product_id',
+  });
+  CartItem.belongsTo(Product, { foreignKey: 'product_id' });
+  Product.hasMany(CartItem, { foreignKey: 'product_id' });
+  CartItem.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasMany(CartItem, { foreignKey: 'user_id' });
+  OrderItem.belongsTo(Order, { foreignKey: 'order_id' });
+  Order.hasMany(OrderItem, { foreignKey: 'order_id' });
+  OrderItem.belongsTo(Product, { foreignKey: 'product_id' });
+  Product.hasMany(OrderItem, { foreignKey: 'product_id' });
+  Order.belongsTo(OrderStatus, { foreignKey: 'status' });
+  OrderStatus.hasMany(Order, { foreignKey: 'status' });
+  Order.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasMany(Order, { foreignKey: 'user_id' });
+  Product.belongsTo(Category, { foreignKey: 'category_id' });
+  Category.hasMany(Product, { foreignKey: 'category_id' });
+  Product.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasMany(Product, { foreignKey: 'user_id' });
+  User.belongsTo(Role, { foreignKey: 'role_id' });
+  Role.hasMany(User, { foreignKey: 'role_id' });
 
   return {
     CartItem,
