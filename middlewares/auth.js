d6d71bde9ed94e3dfb73841e7ff9347701b20c29
Guru Prakash Ram <guruprakashram99@gmail.com>
2020-12-28 10:26:43 +0530

    Define auth middleware.

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const { sequelize } = require('../models');
+const db = require('../models/init-models')(sequelize);
+
+module.exports = async (req, res, next) => {
+  try {
+    console.log('I am here!');
+    const token = req.headers['x-access-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'No token provided!' });
+    }
+
+    const decoded = jwt.verify(token, 'jwtsecretey');
+
+    const user = await db.User.findOne({ where: { email: decoded.email } });
+    if (!user) {
+      return res.status(404).json({ error: "User doesn't exists!" });
+    }
+
+    //next();
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong!!' });
+    console.log(error);
+  }
+};
