77f2b1ace3145a012a4bc6b0dd9413205568922f
Guru Prakash Ram <guruprakashram99@gmail.com>
2020-12-28 16:38:21 +0530

    Assign user to req.loggedInUser. Pass control to next middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,14 +10,15 @@ module.exports = async (req, res, next) => {
       return res.status(401).json({ error: 'No token provided!' });
     }
 
-    const decoded = jwt.verify(token, 'jwtsecretey');
+    const decoded = jwt.verify(token, 'jwtsecretkey');
 
     const user = await db.User.findOne({ where: { email: decoded.email } });
     if (!user) {
       return res.status(404).json({ error: "User doesn't exists!" });
     }
 
-    //next();
+    req.loggedInUser = user.toJSON();
+    next();
   } catch (error) {
     res.status(500).json({ error: 'Something went wrong!!' });
     console.log(error);
