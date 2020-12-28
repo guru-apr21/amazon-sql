76c314de52ec2f17fa62b623183997651a63034e
Guru Prakash Ram <guruprakashram99@gmail.com>
2020-12-28 16:37:01 +0530

    Chnage default role_id value

--- models/User.js
+++ models/User.js
@@ -40,7 +40,7 @@ class User extends Sequelize.Model {
         role_id: {
           type: DataTypes.INTEGER,
           allowNull: false,
-          defaultValue: 2,
+          defaultValue: 1,
           references: {
             model: 'roles',
             key: 'role_id',
